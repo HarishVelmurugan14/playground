96745a455aa9ec9cd07e7b4a77415f38188d2981
harish.vk <harish.vk@zohocorp.com>
2023-07-20 15:57:00 +0530

    Patch Request Sample

---
+++ API/Java/PatchRequest.java
@@ -0,0 +1,86 @@
+package API.Java;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class PatchRequest {
+
+    public static void PatchRequestInit(){
+        PatchRequestUsingHttpURLConnection();
+    }
+
+
+    public static void PatchRequestUsingHttpURLConnection(){
+        String urlValue = "https://example.com/internalapi/v2/16485289/zsa-16485289/form/Volunteer";
+        //Sample
+        String requestBody = "{\"data\" : {\"set_email_notification\":{\"cc\":[\"abc@abc.com\",\"xyz@xyz.com\"],\"bcc\":[\"xyz@abc.com\",\"abc@xyz.com\"],\"content_type\":\"HTML\",\"subject\":\"Temporary\",\"from\":\"zoho.adminuserid\",\"language\":\"English\",\"template_type\":\"inline\",\"to\":[\"harish.vk@zohocorp.com\",\"harish.vk@zohotest.com\"],\"message\":\"Dummy\",\"include_field_values\":true},\"has_email_notification\":false}}";
+        allowMethods("PATCH");
+        PatchRequest(urlValue, requestBody);
+    }
+
+    public static HttpURLConnection PatchRequestBuilder(HttpURLConnection connection){
+        connection.setRequestProperty("Authorization", "Bearer 16425917-6d18bcb4-a19493e369c697c1f457890dbd780fe7");
+        connection.setRequestProperty("Content-Type", "application/json");
+        return connection;
+    }
+
+    @SuppressWarnings("ReassignedVariable")
+    public static void  PatchRequest(String urlValue, String requestBody){
+        try {
+            URL url = new URL(urlValue);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PATCH");
+            PatchRequestBuilder(connection);
+            connection.setDoOutput(true);
+            connection.setDoOutput(true);
+
+            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                outputStream.writeBytes(requestBody);
+                outputStream.flush();
+            }
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                System.out.println("Response: " + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void allowMethods(String... methods) {
+        try {
+            Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(methodsField, methodsField.getModifiers() & ~Modifier.FINAL);
+
+            methodsField.setAccessible(true);
+
+            String[] oldMethods = (String[]) methodsField.get(null);
+            Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
+            methodsSet.addAll(Arrays.asList(methods));
+            String[] newMethods = methodsSet.toArray(new String[0]);
+
+            methodsField.set(null/*static field*/, newMethods);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
