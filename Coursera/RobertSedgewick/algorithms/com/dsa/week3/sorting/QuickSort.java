7109f654a9ec8eb3e5c27fac9bfe108d0a421b12
harish.vk <harish.vk@zohocorp.com>
2023-10-17 10:09:11 +0530

    Quick Sort - Remaining

--- Coursera/RobertSedgewick/algorithms/com/dsa/week3/sorting/QuickSort.java
+++ Coursera/RobertSedgewick/algorithms/com/dsa/week3/sorting/QuickSort.java
@@ -1,8 +1,15 @@
 package Coursera.RobertSedgewick.algorithms.com.dsa.week3.sorting;
 
+import java.util.Arrays;
+
 @SuppressWarnings("ALL")
 public class QuickSort {
     public static int COUNTER = 0;
+
+//    public static void main(String[] args) {
+//        Integer[] a  = {3,9,1,8,2,3,7,6,3,0,2,4};
+//        ThreeWayPartition(a,1,a.length-1);
+//    }
     public static void standardPartition(Comparable[] a, int low, int high){
 
         // No swapping of pivot ; if less increment low; if high pause and proceed reverse path
@@ -32,11 +39,13 @@ public class QuickSort {
         // Swapping of pivot exist ; if less swap with pivot increment low and pivot; if greater swap with high , decreement high and proceed in forward direction
         // If equal increement low
         // if high less than low end
+        int lowCopy = low;
+        int highCopy = high;
 
         int pivot = 0;
         while(high>=low){
             COUNTER++;
-            //System.out.println(Arrays.toString(a));
+            System.out.println(Arrays.toString(a));
             if(a[low].compareTo(a[pivot]) < 0){
                 SortingBaseClass.swap(a,low,pivot);
                 pivot++;
@@ -48,6 +57,9 @@ public class QuickSort {
                 low++;
             }
         }
+        ThreeWayPartition(a,lowCopy,pivot-1);
+        ThreeWayPartition(a,high+1,highCopy);
+
         System.out.println("High - "+high + " Low - " + low + " Pivot - "+pivot);
     }
 
