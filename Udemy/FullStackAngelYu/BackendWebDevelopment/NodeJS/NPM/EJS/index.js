4886fc531a481c32ef803fb2f0a6929528b9ae1e
harish.vk <harish.vk@zohocorp.com>
2023-12-11 18:53:09 +0530

    Javascript Node NPM and EJS

---
+++ Udemy/FullStackAngelYu/BackendWebDevelopment/NodeJS/NPM/EJS/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+let entireContent = "Hi there! It is a weekday. Work properly";
+function findContent(req,res,next) {
+    const d = new Date();
+    let day = d.getDay();
+    if(day === 0 || day === 6){
+        entireContent = "Hi there! It is a weekend. Have Fun";
+    }
+    next();
+}
+
+app.use(findContent);
+app.get('/', (req, res) => {
+    res.render('render.ejs', {entireContent: entireContent});
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
