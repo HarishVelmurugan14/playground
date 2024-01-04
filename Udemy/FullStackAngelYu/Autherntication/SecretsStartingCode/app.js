5342f57c7be8a9c885343b7afdf76628ad106628
harish.vk <harish.vk@zohocorp.com>
2024-01-04 08:22:35 +0530

    Level 4 Encryption - Hashing & Salting

--- Udemy/FullStackAngelYu/Autherntication/SecretsStartingCode/app.js
+++ Udemy/FullStackAngelYu/Autherntication/SecretsStartingCode/app.js
@@ -2,11 +2,13 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
-import md5 from "md5";
+// import encrypt from "mongoose-encryption";
+// import md5 from "md5";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,20 +31,37 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser
-    .save()
-    .then((savedUser) => {
-      console.log("User saved successfully:", savedUser);
-      res.render("secrets.ejs");
-    })
-    .catch((error) => {
-      console.error("Error saving user:", error);
-      res.send("ERROR");
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+    newUser
+      .save()
+      .then((savedUser) => {
+        console.log("User saved successfully:", savedUser);
+        res.render("secrets.ejs");
+      })
+      .catch((error) => {
+        console.error("Error saving user:", error);
+        res.send("ERROR");
+      });
+  });
+
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     password: md5(req.body.password),
+  //   });
+  //   newUser
+  //     .save()
+  //     .then((savedUser) => {
+  //       console.log("User saved successfully:", savedUser);
+  //       res.render("secrets.ejs");
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error saving user:", error);
+  //       res.send("ERROR");
+  //     });
 });
 
 app.get("/login", (req, res) => {
@@ -51,17 +70,21 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(parseInt(req.body.password));
+  //   const password = md5(parseInt(req.body.password));
+  const password = req.body.password;
 
   User.findOne({ email: username })
     .then((foundUser) => {
       //console.log("Found user", foundUser);
       console.log("Match found");
-      if (foundUser.password === password) {
-        res.render("secrets.ejs");
-      }else{
-        console.log("No match");
-      }
+      bcrypt.compare(password, foundUser.password, function (err, result) {
+        //   if (foundUser.password === password) {
+        if (result) {
+          res.render("secrets.ejs");
+        } else {
+          console.log("No match");
+        }
+      });
     })
     .catch((error) => {
       console.error("Error finding user:", error);
