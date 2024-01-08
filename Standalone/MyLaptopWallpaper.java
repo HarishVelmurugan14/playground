c70a223e06aa3a7f1550ee76692a0a88d2bcbac5
harish.vk <harish.vk@zohocorp.com>
2024-01-08 12:24:31 +0530

    Revamp Git and including dependency directory

--- Standalone/MyLaptopWallpaper.java
+++ Standalone/MyLaptopWallpaper.java
@@ -1,14 +1,18 @@
+package Standalone;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 public class MyLaptopWallpaper {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String name = "magic";
-        Boolean myAvailability = false;
-        ArrayList<String> possibleLocations = new ArrayList<String>();
+        boolean myAvailability = false;
+        ArrayList<String> possibleLocations = new ArrayList<>();
         possibleLocations.add("Conferance Room");
         possibleLocations.add("Pantry");
         possibleLocations.add("South Plaza Tent");
         possibleLocations.add("My Home");
-        String whenCanYouExpectMeInMyPlace = "I am not sure";
+        String whenCanYouExpectMe = "I am not sure";
         String emailAddress = "magic.vk@zohocorp.com";
 
 
@@ -17,11 +21,11 @@ public class MyLaptopWallpaper {
             System.out.println("I am not available now. I may be in one of the following places :" + possibleLocations);
             System.out.println("The time by which you can expect me "+ whenCanYouExpectMe);
             System.out.println("Are you ready to wait?");
-            Boolean isVisitorReadyToWait = userInput.nextBoolean();
+            boolean isVisitorReadyToWait = userInput.nextBoolean();
             if(isVisitorReadyToWait) {
                 System.out.println("Great! In the meantime you can find the bug I left for you to find in this code.");
             }else{
-                Systen.out.println("I understand ! Please ping me at "+ emailAddress + ". We can catch up once I am available.");
+                System.out.println("I understand ! Please ping me at "+ emailAddress + ". We can catch up once I am available.");
             }
         }else{
             System.out.println("You shouldn't have come here. I am available.");
