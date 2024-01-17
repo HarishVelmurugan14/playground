f2acab4027623b6dc3d6fe47daf9dfd1bd2e8b30
HarishVelmurugan14 <116337336+HarishVelmurugan14@users.noreply.github.com>
2024-01-17 19:00:25 +0530

    Delete Udemy/SpringIn28Mins directory

--- Udemy/SpringIn28Mins/Basics/src/main/java/com/spring/learn/myartifact/BubbleSortAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.spring.learn.myartifact;
-
-public class BubbleSortAlgorithm implements SortAlgorithm{
-
-	public int[] sort(int[] numbers) {
-		//Assume that bubble sort logic is included
-		return numbers;
-	}
-}
--- Udemy/SpringIn28Mins/Basics/src/main/java/com/spring/learn/myartifact/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.spring.learn.myartifact;
-
-public interface SortAlgorithm {
-	int[] sort(int[] numbers);
-}
