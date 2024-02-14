81d5775fa3919015fc16edf1234512bcedfa7078
harish.vk <harish.vk@zohocorp.com>
2024-02-14 10:31:34 +0530

    JUnit Basics 1

--- Udemy/SpringIn28Mins/JUnitTest/MyMathTest.java
+++ Udemy/SpringIn28Mins/JUnitTest/MyMathTest.java
@@ -10,14 +10,25 @@ public class MyMathTest {
     private final MyMath myMath = new MyMath();
 
     @BeforeAll // @AfterAll
-    void beforeAll(){}
+    static void beforeAll(){
+        System.out.println("BeforeAll");
+    }
 
     @BeforeEach // @AfterEach
-    void beforeEach(){}
+    void beforeEach(){
+        System.out.println("BeforeEach");
+    }
 
     @Test
     void calculate_three(){
+        System.out.println("Three Member Array");
         Assertions.assertEquals(33, myMath.calculateSum(new int[]{10, 11, 12}));
+    }
+
+    @Test
+    void calculate_zero(){
+        System.out.println("Zero Length Array");
+        Assertions.assertEquals(0, myMath.calculateSum(new int[]{}));
     }
 
 }
