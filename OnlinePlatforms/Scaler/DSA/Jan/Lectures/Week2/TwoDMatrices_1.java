f5b0cd458b55e92bb2d073eef28b290b62067a26
harish.vk <harish.vk@zohocorp.com>
2025-01-14 22:00:36 +0530

    2D Matrices - 2

--- OnlinePlatforms/Scaler/DSA/Jan/Lectures/Week2/TwoDMatrices_1.java
+++ OnlinePlatforms/Scaler/DSA/Jan/Lectures/Week2/TwoDMatrices_1.java
@@ -18,7 +18,7 @@ public class TwoDMatrices_1 {
 
 
         // Call Stack
-        TwoDMatrices twoDMatrices = new TwoDMatrices();
+        TwoDMatrices_1 twoDMatrices = new TwoDMatrices_1();
 //        int[][] matrixArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 //        int[][] matrixArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}};
         int[][] matrixArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
