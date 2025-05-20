ec319ef8b31adb8c5d5270d484a1deb1d5c36c27
harish.vk <harish.vk@zohocorp.com>
2025-05-20 22:11:08 +0530

    Graphs Introduction

--- OnlinePlatforms/Scaler/DSA/Advanced/Part4/Advanced_DSA_Part4.java
+++ OnlinePlatforms/Scaler/DSA/Advanced/Part4/Advanced_DSA_Part4.java
@@ -13,6 +13,9 @@ public class Advanced_DSA_Part4 {
         d53GraphsIntroduction.cycleDetectionInAGraph(d53GraphsIntroduction.InputWithCycleNodes, d53GraphsIntroduction.InputWithCycle); //Q1
         d53GraphsIntroduction.pathInADirectedGraph(0, null); //LC 1971 //Q2
 
+        d53GraphsIntroduction.firstDepthFirstSearch(new int[]{1, 1, 2}, 2, 1); //AQ1
+        d53GraphsIntroduction.maximumDepth(5, new int[]{1, 4, 3, 1}, new int[]{5, 2, 4, 4}, new int[]{7, 38, 27, 37, 1}, new int[]{1, 1, 2}, new int[]{32, 18, 26}); //AQ2
+
         /* ------------------------- Day 38 - DSA : Linked List Sorting & Problems ------------------------------*/
 
 
--- OnlinePlatforms/Scaler/DSA/Advanced/Part4/d53_Graphs_Introduction.java
+++ OnlinePlatforms/Scaler/DSA/Advanced/Part4/d53_Graphs_Introduction.java
@@ -3,7 +3,9 @@ package OnlinePlatforms.Scaler.DSA.Advanced.Part4;
 import Resources.Utilities.PrintHelper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Harish Velmurugan
@@ -18,6 +20,8 @@ public class d53_Graphs_Introduction {
     public static int InputWithoutCycleNodes = 4;
     public static int[][] InputWithoutCycle = {{1, 2}, {2, 3}, {3, 4}};
     private final PrintHelper printHelper = new PrintHelper();
+    /* Section : ----------------------------------- [ Approaches ] ------------------------------------ */
+    public int MaxLevel = 0;
 
     public static void main(String[] args) {
 
@@ -27,11 +31,110 @@ public class d53_Graphs_Introduction {
         // Call Stack
         d53_Graphs_Introduction d53GraphsIntroduction = new d53_Graphs_Introduction();
 
-        System.out.println(d53GraphsIntroduction.cycleDetectionInAGraph(InputWithCycleNodes, InputWithCycle));
-        System.out.println(d53GraphsIntroduction.cycleDetectionInAGraph(InputWithoutCycleNodes, InputWithoutCycle));
+//        System.out.println(d53GraphsIntroduction.cycleDetectionInAGraph(InputWithCycleNodes, InputWithCycle));
+//        System.out.println(d53GraphsIntroduction.cycleDetectionInAGraph(InputWithoutCycleNodes, InputWithoutCycle));
+
+        d53GraphsIntroduction.maximumDepth(5, new int[]{1, 4, 3, 1}, new int[]{5, 2, 4, 4}, new int[]{7, 38, 27, 37, 1}, new int[]{1, 1, 2}, new int[]{32, 18, 26});
     }
 
-    /* Section : ----------------------------------- [ Approaches ] ------------------------------------ */
+    public int[] maximumDepth(int A, int[] B, int[] C, int[] D, int[] E, int[] F) {
+        Map<Integer, List<Integer>> adj = buildAdjacencyMatrixMaxDepth(A, B, C);
+        boolean[] visited = new boolean[A + 1];
+        Map<Integer, List<Integer>> levelList = new HashMap<>();
+
+        dfs(1, adj, visited, 0, levelList);
+//        for (int i = 0; i <= 5; i++) {
+//            if (levelList.containsKey(i)) {
+//                System.out.println("LEVEL ->" + i);
+//                System.out.println(levelList.get(i).toString());
+//            }
+//        }
+        int[] result = new int[E.length];
+        for (int i = 0; i < E.length; i++) {
+            int res = -1;
+            int level = E[i] % (MaxLevel + 1);
+            int threshold = F[i];
+            for (int node : levelList.get(level)) {
+                if (D[node] >= threshold) {
+                    res = Math.max(res, D[node]);
+                }
+            }
+            result[i] = res;
+        }
+        return result;
+    }
+
+    public void dfs(int node, Map<Integer, List<Integer>> adj, boolean[] visited, int level, Map<Integer, List<Integer>> levelList) {
+        visited[node] = true;
+        levelList.computeIfAbsent(level, _ -> new ArrayList<>()).add(node);
+
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                MaxLevel = Math.max(MaxLevel, level + 1);
+                dfs(neighbour, adj, visited, level + 1, levelList);
+            }
+        }
+    }
+
+    public Map<Integer, List<Integer>> buildAdjacencyMatrixMaxDepth(int A, int[] B, int[] C) {
+
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+
+        for (int i = 0; i < A - 1; i++) {
+            int u = B[i];
+            int v = C[i];
+            adj.computeIfAbsent(u, _ -> new ArrayList<>()).add(v);
+            adj.computeIfAbsent(v, _ -> new ArrayList<>()).add(u);
+        }
+        return adj;
+    }
+
+    public int firstDepthFirstSearch(int[] A, final int B, final int C) {
+        // Complexity : Time : [ O(V) ]
+        // Complexity : Space : [ O(V) ]
+        List<List<Integer>> adj = buildAdjacencyMatrixTownsProblem(A);
+        boolean[] visited = new boolean[A.length + 1];
+
+        return dfsTownsProblem(C, adj, visited, B) ? 1 : 0;
+
+    }
+
+    public boolean dfsTownsProblem(int node, List<List<Integer>> adj, boolean[] visited, int destination) {
+        if (node == destination) {
+            return true;
+        }
+        visited[node] = true;
+
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                if (dfsTownsProblem(neighbour, adj, visited, destination)) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public List<List<Integer>> buildAdjacencyMatrixTownsProblem(int[] A) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+//        NOTE : BEST WAY **
+//        Map<Integer, List<Integer>> adj = new HashMap<>();
+//        adj.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+
+        for (int i = 0; i < A.length + 1; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i < A.length; i++) {
+            int u = A[i];
+            int v = i + 1;
+            adj.get(u).add(v);
+        }
+        return adj;
+    }
 
 
     public int pathInADirectedGraph(int A, int[][] B) {
@@ -62,8 +165,8 @@ public class d53_Graphs_Introduction {
     }
 
     public List<List<Integer>> buildAdjacencyListForDirectedGraph(int numberOfNodes, int[][] connections) {
-        // Complexity : Time : [ O(A) ]
-        // Complexity : Space : [ O() ]
+        // Complexity : Time : [ O(V+E) ]
+        // Complexity : Space : [ O(V+E) ]
         List<List<Integer>> adj = new ArrayList<>();
 
         for (int i = 0; i <= numberOfNodes; i++) {
