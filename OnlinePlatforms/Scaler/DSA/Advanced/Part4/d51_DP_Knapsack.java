ce1fd9902c9a93085f278eafda2de4de6bc401e9
harish.vk <harish.vk@zohocorp.com>
2025-05-25 10:09:44 +0530

    DP Knapsack

--- OnlinePlatforms/Scaler/DSA/Advanced/Part4/d51_DP_Knapsack.java
+++ OnlinePlatforms/Scaler/DSA/Advanced/Part4/d51_DP_Knapsack.java
@@ -2,6 +2,8 @@ package OnlinePlatforms.Scaler.DSA.Advanced.Part4;
 
 import Resources.Utilities.PrintHelper;
 
+import java.util.HashMap;
+
 /**
  * @author Harish Velmurugan
  * @last-modified 24-05-2025
@@ -19,13 +21,79 @@ public class d51_DP_Knapsack {
         // Call Stack
         d51_DP_Knapsack d51DpKnapsack = new d51_DP_Knapsack();
         System.out.println(d51DpKnapsack.maxValueForKnapsack(new int[]{60, 100, 120}, new int[]{10, 20, 30}, 50));
-
-
+        System.out.println(d51DpKnapsack.unboundedKnapsack(10, new int[]{6, 7}, new int[]{5, 5}));
+        System.out.println(d51DpKnapsack.unboundedKnapsackMapApproach(10, new int[]{6, 7}, new int[]{5, 5}));
     }
 
     /* Section : ----------------------------------- [ Approaches ] ------------------------------------ */
+    public int unboundedKnapsackMapApproach(int A, int[] B, int[] C) {
+        HashMap<String, Integer> memory = new HashMap<>();
+        return unboundedKnapsackMapApproach(A, B.length - 1, B, C, memory);
+    }
+
+    public String getKey(int index, int capacity) {
+        return index + "," + capacity;
+    }
+
+    public int unboundedKnapsackMapApproach(int capacity, int index, int[] values, int[] weights, HashMap<String, Integer> memory) {
+        if (index == 0) {
+            if (capacity >= weights[0]) {
+                return values[0];
+            } else {
+                return 0;
+            }
+        }
+        if (memory.containsKey(getKey(index, capacity))) {
+            return memory.get(getKey(index, capacity));
+        }
+
+        // Option 1 : Not Select
+        int ans = unboundedKnapsackMapApproach(capacity, index - 1, values, weights, memory);
+        // Option 2 : Select
+        if (capacity >= weights[index]) {
+            ans = Math.max(ans, values[index] + unboundedKnapsackMapApproach(capacity - weights[index], index, values, weights, memory));
+        }
+        memory.put(getKey(index, capacity), ans);
+        return memory.get(getKey(index, capacity));
+    }
+
+    public int unboundedKnapsack(int A, int[] B, int[] C) {
+        //NOTE: HK1007
+        int[][] memory = new int[B.length + 1][A + 1];
+        for (int i = 0; i <= B.length; i++) {
+            for (int j = 0; j <= A; j++) {
+                memory[i][j] = -1;
+            }
+        }
+        return unboundedKnapsack(A, B.length - 1, B, C, memory);
+    }
+
+
+    public int unboundedKnapsack(int capacity, int index, int[] values, int[] weights, int[][] memory) {
+        if (index == 0) {
+            if (capacity >= weights[0]) {
+                return values[0];
+            } else {
+                return 0;
+            }
+        }
+        if (memory[index][capacity] != -1) {
+            return memory[index][capacity];
+        }
+
+        // Option 1 : Not Select
+        int ans = unboundedKnapsack(capacity, index - 1, values, weights, memory);
+        // Option 2 : Select
+        if (capacity >= weights[index]) {
+            ans = Math.max(ans, values[index] + unboundedKnapsack(capacity - weights[index], index, values, weights, memory));
+        }
+        memory[index][capacity] = ans;
+        return memory[index][capacity];
+    }
+
 
     public int maxValueForKnapsack(int[] A, int[] B, int C) {
+        //NOTE: HK1006
         int[][] memory = new int[A.length + 1][C + 1];
         for (int i = 0; i <= A.length; i++) {
             for (int j = 0; j <= C; j++) {
